66b2d6f75e9902ceb530168fd70f75a1c24d8128
Faith Magras <bdanielle092@gmail.com>
2021-02-23 12:07:03 -0600

    working on edit

--- TaskIt/client/src/components/Board/BoardEditForm.js
+++ TaskIt/client/src/components/Board/BoardEditForm.js
@@ -10,63 +10,38 @@ import {
     Input,
     Button,
 } from "reactstrap";
+import { BoardContext } from "../../providers/BoardProvider";
 
 
 
 const BoardEditForm = () => {
     const { getToken } = useContext(UserProfileContext)
+    const { getBoardById, updateBoard, board } = useContext(BoardContext)
+    //UseParams pulls in the id information from applications view 
     const { id } = useParams();
     const history = useHistory();
-    //this is a empty string but when the page initially gets loaded then the string will be updated with the current name of the board
-    const [boardToEdit, setBoardToEdit] = useState("")
-    //this the existing board object that is gets loads ar initial load too.
-    const [existingBoard, setExistingBoard] = useState({})
+    const [editBoard, setEditBoard] = useState({ name: "" });
 
-    //this is getting the current state of the board base off the id passed in the uri and setting existingBoard and boardToEdit
     useEffect(() => {
-        getToken()
-            .then((token) =>
-                fetch(`/api/board/${id}`, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                })
-            )
-            .then((res) => res.json())
-            .then((board) => {
-                setExistingBoard(board)
-                setBoardToEdit(board["name"])
-            });
+        getBoardById(parseInt(id))
+    }, [])
 
-    }, []);
+    useEffect(() => {
+        setEditBoard(board)
+    }, [board]);
+
 
     //updating boardToEdit value. Updates boardToEdit value on every key stroke for the input field
     const handleSubmit = (evt) => {
         const newBoard = evt.target.value;
-        setBoardToEdit(newBoard);
+        setEditBoard(newBoard);
     };
 
     // update function to update the database with the new state of the board name
-    const updateBoard = (board) => {
-        getToken()
-            .then((token) =>
-                fetch(`/api/board/${id}`, {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                    body: JSON.stringify({
-                        //keeping all the existing keys the same except name
-                        "active": existingBoard["active"],
-                        "id": existingBoard["id"],
-                        "name": boardToEdit,
-                        "userProfile": existingBoard["userProfile"],
-                        "userProfileId": existingBoard["userProfileId"]
-                    }),
-                })
-            )
+    const editBoard = (event) => {
+        updateBoard({
+            name: editBoard.name
+        })
             .then((evt) => history.push("/"));
     };
 
